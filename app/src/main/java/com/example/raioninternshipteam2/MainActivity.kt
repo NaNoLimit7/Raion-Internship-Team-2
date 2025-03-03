0b01046c87cac32810f6256fbc1b5ac358d5c7e8
Ghani <ghanibaskaraa111@gmail.com>
2025-03-03 14:50:45 +0700

    Inisialisasi Navigasi Pertama & Gambaran Kasar Screen User

--- app/src/main/java/com/example/raioninternshipteam2/MainActivity.kt
+++ app/src/main/java/com/example/raioninternshipteam2/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.raioninternshipteam2.presentation.registration.LoginScreenCafe
+import com.example.raioninternshipteam2.presentation.registration.LoginScreenUser
+import com.example.raioninternshipteam2.presentation.registration.UserOption
 import com.example.raioninternshipteam2.ui.theme.RaionInternshipTeam2Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,14 +21,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RaionInternshipTeam2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val navController = rememberNavController()
+
+            NavHost(navController, startDestination = "UserOption", builder = {
+                composable("UserOption"){
+                    UserOption(navController)
                 }
-            }
+                composable("LoginScreenUser"){
+                    LoginScreenUser(navController)
+                }
+                composable("LoginScreenCafe"){
+                    LoginScreenCafe(navController)
+                }
+            })
+
+
         }
     }
 }
